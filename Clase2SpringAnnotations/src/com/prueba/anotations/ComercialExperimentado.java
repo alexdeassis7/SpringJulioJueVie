e9c8208b9a269b4161a474eae228d84f6d987b59
Alex <alexdeassis7@gmail.com>
2021-07-16 22:01:32 -0300

    Clase 2 Demo Anotation INCOMPLETO

---
+++ Clase2SpringAnnotations/src/com/prueba/anotations/ComercialExperimentado.java
@@ -0,0 +1,19 @@
+package com.prueba.anotations;
+
+import com.interfaces.IEmpleados;
+
+public class ComercialExperimentado implements IEmpleados{
+
+	@Override
+	public String getTareas() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getinforme() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ Clase2SpringAnnotations/src/com/prueba/anotations/InformeFinancieroTrimestre1.java
@@ -0,0 +1,5 @@
+package com.prueba.anotations;
+
+public class InformeFinancieroTrimestre1 {
+
+}
